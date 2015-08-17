567f8620c7052fee2e25c149e1f03e78669c90ee
Iakov Kirilenko <iakov@trikset.com>
2015-08-17 15:51:05 +0300

    Remove colon from hostname

--- recipes-trik/trik-hostname/files/trik-hostname.sh
+++ recipes-trik/trik-hostname/files/trik-hostname.sh
@@ -11,7 +11,7 @@ if [ ! -d /etc/trik/custom ]; then
         mkdir -p /etc/trik/custom/
 fi
 if [ ! -f /etc/trik/custom/hostname ]; then
-        echo "trik-$(cat /sys/class/net/wlan0/address | tail -c 9)" > /etc/trik/custom/hostname
+        echo "trik-$(cat /sys/class/net/wlan0/address | tail -c 9 | sed 's/://g')" > /etc/trik/custom/hostname
 fi
 
 if [ ! -L /etc/hostname ]; then
